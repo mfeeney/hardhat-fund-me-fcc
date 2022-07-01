7c58723ffd36c641f93b942f075eec7fc8b639fc
Matthew Feeney <mattf@edispatches.com>
2022-07-01 14:47:58 -0500

    Initial commit.

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,40 @@
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if the contract doesn't exist, we deploy a minimal version of it
+    // for our local testing
+
+    // what happens when we change chains?
+    // when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], // put price feed address
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("------------------------------------------------------------")
+}
+
+module.exports.tags = ["all", "fundme"]
